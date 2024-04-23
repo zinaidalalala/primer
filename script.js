8acd9757bc25226c9563af97635cd37fcc0b0736
Zinaida Danelian <143716592+zinaidalalala@users.noreply.github.com>
2024-04-23 21:08:14 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// let options = {
+//   root: null,
+//   rootMargin: '0px',
+//   threshold: 0.5
+// };
+
+// let callback = (entries, observer) => {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       entry.target.style.opacity = '1';
+//       entry.target.style.transition = 'opacity 1s';
+//     } else {
+//       entry.target.style.opacity = '0';
+//       entry.target.style.transition = 'opacity 1s';
+//     }
+//   });
+// };
+
+// let observer = new IntersectionObserver(callback, options);
+
+// let target = document.querySelector('.firstPart');
+// observer.observe(target);
